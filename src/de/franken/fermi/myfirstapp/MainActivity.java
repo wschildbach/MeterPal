dfe503d943173bb1119d773103d28a4db1d119f5
Wolfgang Schildbach <github@fermi.franken.de>
2013-05-14 15:15:29 +0200

    first working version

---
+++ src/de/franken/fermi/myfirstapp/MainActivity.java
@@ -0,0 +1,35 @@
+package de.franken.fermi.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {     // Do something in response to button }
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.editText1);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent); 
+    }
+}
