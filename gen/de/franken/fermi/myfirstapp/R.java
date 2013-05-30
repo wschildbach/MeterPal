01a88398dece58b6f322469d8d4a84db412de76a
Wolfgang Schildbach <github@fermi.franken.de>
2013-05-30 23:06:54 +0200

    added lightbulb switch

--- gen/de/franken/fermi/myfirstapp/R.java
+++ gen/de/franken/fermi/myfirstapp/R.java
@@ -23,17 +23,18 @@ public final class R {
     public static final class drawable {
         public static final int ic_activearea=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int img_gasmeter=0x7f020002;
+        public static final int ic_lightbulb=0x7f020002;
+        public static final int img_gasmeter=0x7f020003;
     }
     public static final class id {
         public static final int MeterID=0x7f080005;
         public static final int MeterName=0x7f080004;
         public static final int action_settings=0x7f08000e;
-        public static final int cameraButton=0x7f08000c;
+        public static final int activeArea=0x7f08000c;
+        public static final int cameraButton=0x7f08000b;
         public static final int editText1=0x7f080001;
         public static final int hello_world=0x7f080000;
-        public static final int imageView1=0x7f08000d;
-        public static final int lightSwitch=0x7f08000b;
+        public static final int lightBulbButton=0x7f08000d;
         public static final int linearLayout1=0x7f080002;
         public static final int meterSnapshot=0x7f08000a;
         public static final int meterTakenDate=0x7f080008;
