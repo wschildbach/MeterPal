18d50e199fc456c5e1c1dfeac91230de8b3f728c
Wolfgang Schildbach <github@fermi.franken.de>
2013-05-30 22:41:14 +0200

    more UI changes   added "plus" button, justification, camera image

--- gen/de/franken/fermi/myfirstapp/R.java
+++ gen/de/franken/fermi/myfirstapp/R.java
@@ -21,20 +21,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_activearea=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int img_gasmeter=0x7f020002;
     }
     public static final class id {
-        public static final int MeterID=0x7f080004;
-        public static final int MeterName=0x7f080001;
-        public static final int action_settings=0x7f08000a;
+        public static final int MeterID=0x7f080005;
+        public static final int MeterName=0x7f080004;
+        public static final int action_settings=0x7f08000e;
+        public static final int cameraButton=0x7f08000c;
+        public static final int editText1=0x7f080001;
         public static final int hello_world=0x7f080000;
+        public static final int imageView1=0x7f08000d;
+        public static final int lightSwitch=0x7f08000b;
         public static final int linearLayout1=0x7f080002;
-        public static final int meterSnapshot=0x7f080009;
-        public static final int meterTakenDate=0x7f080007;
-        public static final int meterTakenTime=0x7f080008;
-        public static final int meterTakenValue=0x7f080005;
+        public static final int meterSnapshot=0x7f08000a;
+        public static final int meterTakenDate=0x7f080008;
+        public static final int meterTakenTime=0x7f080009;
+        public static final int meterTakenValue=0x7f080006;
         public static final int newMeterButton=0x7f080003;
-        public static final int recordMeter=0x7f080006;
+        public static final int recordMeter=0x7f080007;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -52,7 +58,10 @@ public final class R {
         public static final int button_send=0x7f050003;
         public static final int edit_message=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int lightSwitchText=0x7f050009;
         public static final int meterSnapshot=0x7f050008;
+        public static final int meterSnapshotActiveArea=0x7f05000b;
+        public static final int meterSnapshotCamera=0x7f05000a;
         public static final int recordText=0x7f050007;
         public static final int title_activity_display_message=0x7f050004;
         public static final int title_activity_record_counter=0x7f050006;
