49ea1e4270f72031e0bc8261edee8a3882466d76
Wolfgang Schildbach <github@fermi.franken.de>
2013-05-31 00:17:58 +0200

    "new meter" activity

--- gen/de/franken/fermi/myfirstapp/R.java
+++ gen/de/franken/fermi/myfirstapp/R.java
@@ -22,49 +22,64 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_activearea=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_lightbulb=0x7f020002;
-        public static final int img_gasmeter=0x7f020003;
+        public static final int ic_checkmark=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_lightbulb=0x7f020003;
+        public static final int img_gasmeter=0x7f020004;
     }
     public static final class id {
-        public static final int MeterID=0x7f080005;
-        public static final int MeterName=0x7f080004;
-        public static final int action_settings=0x7f08000e;
-        public static final int activeArea=0x7f08000c;
-        public static final int cameraButton=0x7f08000b;
+        public static final int MeterID=0x7f08000b;
+        public static final int MeterName=0x7f08000a;
+        public static final int action_settings=0x7f080014;
+        public static final int activeArea=0x7f080012;
+        public static final int cameraButton=0x7f080011;
+        public static final int counter_ID=0x7f080005;
+        public static final int counter_ID_label=0x7f080003;
+        public static final int counter_name=0x7f080007;
+        public static final int counter_name_label=0x7f080002;
+        public static final int counter_type=0x7f080006;
+        public static final int counter_type_label=0x7f080004;
         public static final int editText1=0x7f080001;
         public static final int hello_world=0x7f080000;
-        public static final int lightBulbButton=0x7f08000d;
-        public static final int linearLayout1=0x7f080002;
-        public static final int meterSnapshot=0x7f08000a;
-        public static final int meterTakenDate=0x7f080008;
-        public static final int meterTakenTime=0x7f080009;
-        public static final int meterTakenValue=0x7f080006;
-        public static final int newMeterButton=0x7f080003;
-        public static final int recordMeter=0x7f080007;
+        public static final int lightBulbButton=0x7f080013;
+        public static final int linearLayout1=0x7f080008;
+        public static final int menu_save=0x7f080015;
+        public static final int meterSnapshot=0x7f080010;
+        public static final int meterTakenDate=0x7f08000e;
+        public static final int meterTakenTime=0x7f08000f;
+        public static final int meterTakenValue=0x7f08000c;
+        public static final int newMeterButton=0x7f080009;
+        public static final int recordMeter=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_record_counter=0x7f030002;
+        public static final int activity_new_meter=0x7f030002;
+        public static final int activity_record_counter=0x7f030003;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
         public static final int main=0x7f070001;
-        public static final int record_counter=0x7f070002;
+        public static final int new_meter=0x7f070002;
+        public static final int record_counter=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050003;
+        public static final int counter_ID_label=0x7f05000e;
+        public static final int counter_name_label=0x7f05000d;
+        public static final int counter_type_label=0x7f05000f;
         public static final int edit_message=0x7f050005;
         public static final int hello_world=0x7f050002;
         public static final int lightSwitchText=0x7f050009;
+        public static final int menu_done=0x7f050010;
         public static final int meterSnapshot=0x7f050008;
         public static final int meterSnapshotActiveArea=0x7f05000b;
         public static final int meterSnapshotCamera=0x7f05000a;
         public static final int recordText=0x7f050007;
         public static final int title_activity_display_message=0x7f050004;
+        public static final int title_activity_new_meter=0x7f05000c;
         public static final int title_activity_record_counter=0x7f050006;
     }
     public static final class style {
