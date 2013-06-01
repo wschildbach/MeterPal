aca4b256b217a6387a63059fd57ac34b2a9576a8
Wolfgang Schildbach <github@fermi.franken.de>
2013-06-01 16:52:35 +0200

    added database connections

--- gen/de/franken/fermi/myfirstapp/R.java
+++ gen/de/franken/fermi/myfirstapp/R.java
@@ -32,29 +32,25 @@ public final class R {
         public static final int img_gasmeter=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000f;
-        public static final int activeArea=0x7f09000d;
-        public static final int buttonDone=0x7f090008;
-        public static final int cameraButton=0x7f09000c;
-        public static final int counter_ID=0x7f090005;
-        public static final int counter_ID_label=0x7f090004;
-        public static final int counter_name=0x7f090003;
-        public static final int counter_name_label=0x7f090002;
-        public static final int counter_type=0x7f090007;
-        public static final int counter_type_label=0x7f090006;
-        public static final int editText1=0x7f090001;
-        public static final int hello_world=0x7f090000;
-        public static final int imageButton1=0x7f09000e;
-        public static final int listView1=0x7f090009;
-        public static final int menu_newmeter=0x7f090010;
-        public static final int meterTakenValue=0x7f09000a;
-        public static final int recordMeter=0x7f09000b;
+        public static final int action_settings=0x7f09000d;
+        public static final int activeArea=0x7f09000b;
+        public static final int buttonDone=0x7f090006;
+        public static final int cameraButton=0x7f09000a;
+        public static final int counter_ID=0x7f090003;
+        public static final int counter_ID_label=0x7f090002;
+        public static final int counter_name=0x7f090001;
+        public static final int counter_name_label=0x7f090000;
+        public static final int counter_type=0x7f090005;
+        public static final int counter_type_label=0x7f090004;
+        public static final int imageButton1=0x7f09000c;
+        public static final int listView1=0x7f090007;
+        public static final int menu_newmeter=0x7f09000e;
+        public static final int meterTakenValue=0x7f090008;
+        public static final int recordMeter=0x7f090009;
     }
     public static final class layout {
-        public static final int activity_display_message=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_new_meter=0x7f030002;
-        public static final int activity_record_counter=0x7f030003;
+        public static final int activity_new_meter=0x7f030000;
+        public static final int activity_record_counter=0x7f030001;
     }
     public static final class menu {
         public static final int display_message=0x7f080000;
@@ -76,6 +72,7 @@ public final class R {
         public static final int meterSnapshot=0x7f050009;
         public static final int meterSnapshotActiveArea=0x7f05000c;
         public static final int meterSnapshotCamera=0x7f05000b;
+        public static final int newMeterName=0x7f050012;
         public static final int newmeter=0x7f050011;
         public static final int recordText=0x7f050008;
         public static final int title_activity_display_message=0x7f050001;
--- src/de/franken/fermi/myfirstapp/RecordDeviceReadingActivity.java
+++ src/de/franken/fermi/myfirstapp/RecordDeviceReadingActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,9 +13,20 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class RecordDeviceReadingActivity extends Activity {
+	/* the activity can have two states:
+	 * ENTER_READING, enters a reading on an existing device.
+	 * ENTER_DEVICE, enter a new device into the database.
+	 */
+	
+	private static final int STATE_ENTER_READING = 0;
+	private static final int STATE_ENTER_DEVICE = 1;
+	private int mState;
+	
 	public final static String EXTRA_MESSAGE = "de.franken.myfirstapp.recordCounterWithName";
     private static final String TAG = "RecordCounterActivity";
 
@@ -74,6 +86,184 @@ public class RecordDeviceReadingActivity extends Activity {
 	         */
 	        public static final String COLUMN_NAME_COUNTER_READATTIME = "readAtTime";
 		}
+	}
+
+    // Handle to a new DatabaseHelper.
+    private DatabaseHelper mOpenHelper;
+    private Long mDeviceID ;
+    private String mDeviceName ;
+    
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_record_counter);
+
+        mOpenHelper = new DatabaseHelper(getApplicationContext());
+
+        // the only intent we have is to record a meter. See if we've been given
+        // a meter ID that is in our database
+		Intent intent = getIntent();
+		String extra = intent.getStringExtra(EXTRA_MESSAGE);
+
+		/* The intent can get the device ID as extra. If there is no extra, or the
+		 * device id does not exist, pick the first available.
+		 * if there is none available, enter a new device.
+		 */
+
+		mState = STATE_ENTER_READING;			
+
+		// XXX I think we should be catching and handling the exception here
+		if (extra != null) {
+			mDeviceID = Long.parseLong(extra);
+			mDeviceName = getCounterName(mDeviceID); // mis-use to see if we have this device
+			if (mDeviceName == null) {
+				extra = null; // discard invalid device ID
+			}
+		}
+
+		if (extra == null) {
+			mDeviceID = getFirstCounterID();			
+			if (mDeviceID == null) {
+		        Resources res = getResources();
+				mDeviceName = new String(res.getString(R.string.newMeterName));
+				mDeviceID = newDevice(mDeviceName);
+				mState = STATE_ENTER_DEVICE;
+			}
+		}
+	}
+
+    /**
+     * This method is called when the Activity is about to come to the foreground. This happens
+     * when the Activity comes to the top of the task stack, OR when it is first starting.
+     *
+     * XXX Explain what we do here.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        switchContentView();
+    }
+
+    private void switchContentView() {
+        setTitle(mDeviceName);
+        switch (mState) {
+        case STATE_ENTER_DEVICE:
+            setContentView(R.layout.activity_new_meter);
+        	break;
+        case STATE_ENTER_READING:
+            setContentView(R.layout.activity_record_counter);
+            break;
+        }
+    }
+
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.record_counter, menu);
+
+		return true;
+	}
+
+    /** Called when the user clicks the submit button after meter data has been entered. */
+    public void newMeterDone(View view) {
+    	SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+    	TextView t ;
+
+    	t = (TextView)findViewById(R.id.counter_name);
+    	mDeviceName = t.getText().toString();
+    	t = (TextView)findViewById(R.id.counter_type);
+    	int type = dbc.dev.METER_TYPE_GAS; // Integer.parseInt(t.getText().toString());
+
+		ContentValues cv = new ContentValues();
+		cv.put(dbc.dev.COLUMN_NAME_METER_NAME, mDeviceName);
+		cv.put(dbc.dev.COLUMN_NAME_METER_TYPE, type);
+
+		db.update(dbc.dev.TABLE_NAME, cv, dbc.dev._ID + "=" + mDeviceID, null);
+
+		mState = STATE_ENTER_READING;
+		switchContentView();
+    }
+
+    public void newReadingDone(View view) {
+    	SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+    	TextView t ;
+    	t = (TextView)findViewById(R.id.meterTakenValue);
+    	double value = Double.parseDouble(t.getText().toString());
+
+    	ContentValues cv = new ContentValues();
+    	cv.put(dbc.entries.COLUMN_NAME_COUNTER_ID, mDeviceID);
+    	cv.put(dbc.entries.COLUMN_NAME_COUNTER_READATTIME, System.currentTimeMillis());
+    	cv.put(dbc.entries.COLUMN_NAME_COUNTER_VALUE, value);
+
+    	db.insertOrThrow(dbc.entries.TABLE_NAME, null, cv);
+    }
+
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_newmeter:
+			// XXX refactor me
+	        Resources res = getResources();
+			mDeviceName = new String(res.getString(R.string.newMeterName));
+			mDeviceID = newDevice(mDeviceName);
+			mState = STATE_ENTER_DEVICE;
+			switchContentView();
+
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private final Long getFirstCounterID()
+	{
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        Cursor c = db.query(true, // unique
+        		dbc.dev.TABLE_NAME, // table name
+        		null, // all columns
+        		null, // select all
+        		null, // no selection args
+        		null, // no groupBy
+        		null, // no having
+        		dbc.dev._ID, // order by _ID
+        		null); // no limit
+
+        if (!c.moveToFirst()) return null;
+
+        return c.getLong(c.getColumnIndexOrThrow(dbc.dev._ID));
+	}
+
+	private final String getCounterName(long ID)
+	{
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        Cursor c = db.query(true,dbc.dev.TABLE_NAME, null, dbc.dev._ID+"="+ID, null,          null,     null,   null, null);
+//      c = db.query      (true, table,            columns, selection, selectionArgs, groupBy, having, orderBy, limit)
+
+        if (!c.moveToFirst()) return null;
+        int pos = c.getColumnIndexOrThrow(dbc.dev.COLUMN_NAME_METER_NAME);
+        String name = c.getString(pos);
+        return name;
+	}
+
+	final void initHistoryView(int ID) {
+		/*
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        // XXXX use dbc.dev._ID
+        Cursor c = db.query(dbc.TABLE_NAME, null, "'"+dbc.COLUMN_NAME_COUNTER_ID + "'='"+ID+"'", null, null, dbc.COLUMN_NAME_COUNTER_READATTIME, null);
+        ListView lv = (ListView) findViewById(R.id.listView1);
+
+        if (!c.moveToFirst()) return ;
+
+        int posVal = c.getColumnIndexOrThrow(dbc.COLUMN_NAME_COUNTER_VALUE);
+        int posTime = c.getColumnIndexOrThrow(dbc.COLUMN_NAME_COUNTER_READATTIME);
+        do {
+        	long time = c.getLong(posTime);
+        	float val = c.getFloat(posVal);
+        } while (c.moveToNext());
+        */
 	}
 	
 	static class DatabaseHelper extends SQLiteOpenHelper {
@@ -156,114 +346,12 @@ public class RecordDeviceReadingActivity extends Activity {
 		}
 	}
 
-    // Handle to a new DatabaseHelper.
-    public DatabaseHelper mOpenHelper;
+	private long newDevice(String s) {
+    	SQLiteDatabase db = mOpenHelper.getWritableDatabase();
 
-    @Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_record_counter);
-
-        // Creates a new helper object. Note that the database itself isn't opened until
-        // something tries to access it, and it's only created if it doesn't already exist.
-        mOpenHelper = new DatabaseHelper(getApplicationContext());
-
-        // the only intent we have is to record a meter. See if we've been given
-        // a meter ID that is in our database
-		Intent intent = getIntent();
-		String extra = intent.getStringExtra(EXTRA_MESSAGE);
-		int counterID ;
-		
-		// default
-		if (extra == null) {
-			// XXX I think we should be catching and handling the exception here
-			counterID = getFirstCounterID();
-		} else {
-			// XXX I think we should be catching and handling the exception here
-			counterID = Integer.parseInt(extra);
-		}
-
-		String name = getCounterName(counterID);
-		if (name == null) {
-			// create a new meter
-			intent = new Intent(this, AddMeterDeviceActivity.class);
-			startActivity(intent); // this may not return...
-		}
-		else
-		{
-			setTitle(name);
-		}
-
-		// XXX we should not be doing this as it calls the database in the UI thread
-        initHistoryView(counterID);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.record_counter, menu);
-
-		return true;
-	}
-
-	/** Called when the user clicks the new Meter button */
-	public void newMeter(View view) { // Do something in response to button }
-		Intent intent = new Intent(this, AddMeterDeviceActivity.class);
-
-		startActivity(intent);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_newmeter:
-			Intent intent = new Intent(this, AddMeterDeviceActivity.class);
-			startActivity(intent);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
-	final int getFirstCounterID()
-	{
-        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-        Cursor c = db.query(true,dbc.dev.TABLE_NAME, null,    null,        null,     null,     null,   dbc.dev._ID, null);
-//        c = db.query     (true, table,            columns, selection, selectionArgs, groupBy, having, orderBy, limit)
-
-        // XXX not really safe as an error return
-        if (!c.moveToFirst()) return -1;
-
-        return c.getInt(c.getColumnIndexOrThrow(dbc.dev._ID));
-	}
-
-	final String getCounterName(int ID)
-	{
-        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-        Cursor c = db.query(true,dbc.dev.TABLE_NAME, null, dbc.dev._ID+"="+ID, null,          null,     null,   null, null);
-//      c = db.query      (true, table,            columns, selection, selectionArgs, groupBy, having, orderBy, limit)
-
-        if (!c.moveToFirst()) return null;
-        int pos = c.getColumnIndexOrThrow(dbc.dev.COLUMN_NAME_METER_NAME);
-        String name = c.getString(pos);
-        return name;
-	}
-
-	final void initHistoryView(int ID) {
-		/*
-        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-        // XXXX use dbc.dev._ID
-        Cursor c = db.query(dbc.TABLE_NAME, null, "'"+dbc.COLUMN_NAME_COUNTER_ID + "'='"+ID+"'", null, null, dbc.COLUMN_NAME_COUNTER_READATTIME, null);
-        ListView lv = (ListView) findViewById(R.id.listView1);
-
-        if (!c.moveToFirst()) return ;
-
-        int posVal = c.getColumnIndexOrThrow(dbc.COLUMN_NAME_COUNTER_VALUE);
-        int posTime = c.getColumnIndexOrThrow(dbc.COLUMN_NAME_COUNTER_READATTIME);
-        do {
-        	long time = c.getLong(posTime);
-        	float val = c.getFloat(posVal);
-        } while (c.moveToNext());
-        */
+    	ContentValues cv = new ContentValues();
+		cv.put(dbc.dev.COLUMN_NAME_METER_NAME, s);
+//		cv.put(dbc.dev.COLUMN_NAME_METER_TYPE, dbc.dev.METER_TYPE_GAS); // XXX should this be hardcoded?
+		return db.insertOrThrow(dbc.dev.TABLE_NAME, null, cv);		
 	}
 }
